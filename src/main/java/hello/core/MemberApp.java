55b3b6463e3d69dda18b9a629bb3dd62b49d7279
lllilllilllilili <hik3562@hufs.ac.kr>
2022-12-17 10:26:07 +0900

    name property 꺼내오기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -11,7 +11,7 @@ public class MemberApp {
       Member member = new Member(1L, "memberA", Grade.VIP);
       memberService.join(member);
       Member findMember = memberService.findMember(1L);
-      System.out.println("new member = " + member);
-      System.out.println("find Member = " + findMember);
+      System.out.println("new member = " + member.getName());
+      System.out.println("find Member = " + findMember.getName());
    }
 }
