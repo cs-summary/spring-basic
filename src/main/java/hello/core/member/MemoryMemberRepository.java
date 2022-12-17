902486966265a67e487e7054d9874dd8ff996f05
lllilllilllilili <hik3562@hufs.ac.kr>
2022-12-17 10:26:56 +0900

    정렬

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -8,9 +8,7 @@ public class MemoryMemberRepository implements MemberRepository{
     private static Map<Long, Member> store = new HashMap();
 
     @Override
-    public void save(Member member) {
-        store.put(member.getId(), member);
-    }
+    public void save(Member member) { store.put(member.getId(), member); }
 
     @Override
     public Member findById(Long memberId) {
