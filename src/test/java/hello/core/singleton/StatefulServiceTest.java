c23e476807aabee660716ca45ee7c62f4909fea5
lllilllilllilili <hik3562@hufs.ac.kr>
2022-12-18 12:53:56 +0900

    싱글톤 필드 공유하지 마세요

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -12,12 +12,11 @@ class StatefulServiceTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
+        int userA = statefulService1.order("userA", 10000);
+        int userB = statefulService2.order("userB", 20000);
+        //int price = statefulService1.getPrice();
 
-        statefulService1.order("userA", 10000);
-        statefulService2.order("userB", 20000);
-        int price = statefulService1.getPrice();
-
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        Assertions.assertThat(userB).isEqualTo(20000);
     }
 
     static class TestConfig {
